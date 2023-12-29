ad3668ed0cd1a71d1e63aa5433e8e48e7179ade7
Myo Thant <jamesrobinson636478@gmail.com>
2023-12-30 00:58:37 +0700

    Add Auth to state managment

--- src/store.js
+++ src/store.js
@@ -1,12 +1,15 @@
 import { legacy_createStore as createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { authReducer } from './Reducers/userReducers';
 
 const reducer = combineReducers({
-
+    auth: authReducer
 });
 
-let initState = {};
+let initState = {
+    
+};
 
 const middleware = [thunk];
 const store = createStore(reducer, initState, composeWithDevTools(applyMiddleware(...middleware)));
