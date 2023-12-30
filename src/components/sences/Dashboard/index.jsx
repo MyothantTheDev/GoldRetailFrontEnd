f26ee2d67b069edc9f23d697dd6bfdc573a5591f
Myo Thant <003.myothant7729@gmail.com>
2023-12-30 23:47:20 +0700

    Finish AdminDashBoard

--- src/components/sences/Dashboard/index.jsx
+++ src/components/sences/Dashboard/index.jsx
@@ -1,6 +1,19 @@
+import Header from "../../Layout/Header";
+import { Box } from "@mui/material";
+import UserDataGrid from "./userDataGrid";
 
 const Dashboard = () => {
-    return <div>Dashboard</div>
+  return (
+    <Box m='20px' >
+      <Box display="flex" justifyContent="space-between" alignItems="center">
+        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+      </Box>
+      <Box height="50vh" width="100%" overflow="scroll">
+        <UserDataGrid />
+        {/* <div>Hello</div> */}
+      </Box>
+    </Box>
+  )
 }
 
-export default Dashboard
+export default Dashboard;
---
+++ src/components/sences/Dashboard/userDataGrid.jsx
@@ -0,0 +1,45 @@
+import { Box, useTheme, IconButton, InputBase } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { tokens } from "../../../theme";
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import DeleteIcon from '@mui/icons-material/DeleteOutlined';
+import {
+    GridActionsCellItem
+  } from '@mui/x-data-grid';
+import ToolBar from "../utils/ToolBar";
+import CustomDataGrid from "../../Layout/DataGrid";
+
+const UserDataGrid = () => {
+  
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
+    // TABLE COLUMS
+
+    const columns = [
+    { field: 'id', headerName: 'ID', width: 150 },
+    { field: 'username' , headerName: 'Student Name', width: 200 }, 
+    { field: 'email', headerName: 'Email', width: 200 },
+    { field: 'role', headerName: 'Role', width: 150 },
+    { field: 'batch', headerName: 'Batch', width: 150 },
+    ];
+
+    // DATA TABLE
+
+    return (
+        <Box m="20px">
+            <Box display="flex" width='30%' bgcolor={colors.primary[400]} sx={{ marginBottom: "30px" }} borderRadius="3px">
+                <InputBase sx={{ ml:2, flex: 1 }} placeholder="Search" />
+                <IconButton type='button' sx={{ p:1 }}>
+                    <SearchOutlinedIcon/>
+                </IconButton>
+            </Box> 
+            <Box >
+              <CustomDataGrid columns={columns} rows={[]} />
+            </Box>
+        </Box>
+    )
+}
+
+export default UserDataGrid;
