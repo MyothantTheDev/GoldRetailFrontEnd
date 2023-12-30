76beab99c3e194b1f303fc16cb7b55ec92ab1cd2
Myo Thant <003.myothant7729@gmail.com>
2023-12-30 23:43:57 +0700

    Finish Admin Home

--- src/components/Home/AdminHome.jsx
+++ src/components/Home/AdminHome.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import { ColorModeContext, useMode } from '../../theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import SideBar from '../sences/Global/SideBar';
+import TopBar from '../sences/Global/TopBar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from '../sences/Dashboard';
 
 const AdminHome = () => {
+  const [theme, colorMode] = useMode();
+
   return (
-    <div>
-      
-    </div>
+    <ColorModeContext.Provider value={colorMode} >
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="Home">
+          <SideBar/>
+          <main className="Content">
+            <TopBar/>
+            <Routes>
+              <Route path='/' element={ <Dashboard /> }/>
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   )
 }
 
