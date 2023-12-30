a457ad7792b1502997eef1bb05b87d1c8738c9e0
Myo Thant <003.myothant7729@gmail.com>
2023-12-31 00:46:41 +0700

    Alert

---
+++ src/components/Layout/Alert.jsx
@@ -0,0 +1,20 @@
+import Alert from "@mui/material/Alert";
+import Stack from "@mui/material/Stack";
+import { useState } from "react";
+
+const NotiAlert = ({ message, severity = 'success' }) => {
+
+  const [toggle, setToggle] = useState('block');
+
+  const closeHandler = () => {
+    toggle === 'block' ? setToggle('none') : setToggle('block')
+  }
+
+  return (
+    <Stack sx={{width: '100%', margin: '20px 0', display: toggle}} spacing={2} >
+      <Alert severity={severity} onClose={closeHandler} >{message}</Alert>
+    </Stack>
+  )
+}
+
+export default NotiAlert;
