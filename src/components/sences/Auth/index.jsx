706435bbb50fa092322758fc6b77118865cfa07b
Myo Thant <003.myothant7729@gmail.com>
2023-12-30 23:46:45 +0700

    styling change

--- src/components/sences/Auth/index.jsx
+++ src/components/sences/Auth/index.jsx
@@ -1,32 +1,34 @@
-import './LoginForm.css'
 import React from "react"
 import { FaUser } from '@react-icons/all-files/fa/FaUser'
 import { IoMdLock } from '@react-icons/all-files/io/IoMdLock'
+import './LoginForm.css'
 
 const LoginForm = () => {
 	return (
 		<>
-			<div className="LoginForm">
-				<div className="wrapper">
-					<form action="">
-						<h1>Login</h1>
-						<div className="input-box">
-							<input type="text" placeholder="Username" required/>
-							<FaUser className="icon" />
-						</div>
-						<div className="input-box">
-							<input type="password" placeholder="Password" required/>
-							<IoMdLock className="icon" />
-						</div>
+			<div className="App">
+				<div className="LoginForm">
+					<div className="wrapper">
+						<form action="">
+							<h1>Login</h1>
+							<div className="input-box">
+								<input type="text" placeholder="Username" required/>
+								<FaUser className="icon" />
+							</div>
+							<div className="input-box">
+								<input type="password" placeholder="Password" required/>
+								<IoMdLock className="icon" />
+							</div>
 
-						<div className="remember">
-							<label >
-								<input type="checkbox" required/>Remember me
-							</label>
-						</div>
+							<div className="remember">
+								<label >
+									<input type="checkbox" required/>Remember me
+								</label>
+							</div>
 
-						<button type="submit">Login</button>
-					</form>
+							<button type="submit">Login</button>
+						</form>
+					</div>
 				</div>
 			</div>
 		</>
