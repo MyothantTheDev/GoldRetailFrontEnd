68e12c9da66b3c0b6f52c2052f92191fa837dd85
Myo Thant <003.myothant7729@gmail.com>
2023-12-30 23:42:32 +0700

    Add more styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import LoginForm from './components/sences/Auth'
+import AdminHome from './components/Home/AdminHome'
 
 function App() {
 
   return (
     <>
-    <div className="App">
+    <div className="app">
       <Router>
         <Routes>
           <Route path='/' Component={LoginForm}/>
+          <Route path='/account' >
+            <Route path='admin/*' Component={AdminHome} />
+          </Route>
         </Routes>
       </Router>
     </div>
